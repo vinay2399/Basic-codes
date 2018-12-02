5cf50140a7e74b59a6afa87fa418b40877a4a7da
vinay2399 <vickysharma@vickys-MacBook-Air-3.local>
2018-12-02 20:57:08 +0530

    palindrome program added

--- b.java
+++ b.java
@@ -1,1 +1,19 @@
-edit content of b.java
+// Function to print decimal palindromic number 
+void generatePalindromes(int n) 
+{ 
+    int number; 
+  
+    // Run two times for odd and even length palindromes 
+    for (int j = 0; j < 2; j++) 
+    { 
+        // Creates palindrome numbers with first half as i.  
+        // Value of j decided whether we need an odd length 
+        // of even length palindrome. 
+        int i = 1; 
+        while ((number = createPalindrome(i, 10, j % 2)) < n) 
+        { 
+            cout << number << " "; 
+            i++; 
+        } 
+    } 
+} 
